a2db0268686ea43badc327ce7fbe1e60460b153b
Travers Ching <traversc@gmail.com>
2026-01-20 06:10:02 +0000

    version 0.1.7

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -21,10 +21,7 @@ IS_GITHUB <- Sys.getenv("IS_GITHUB") != ""
 # install.packages("qs2")
 
 ## ----eval=FALSE---------------------------------------------------------------
-# remotes::install_cran("qs2", type = "source", configure.args = "--with-TBB --with-simd=AVX2")
-
-## ----eval=FALSE---------------------------------------------------------------
-# remotes::install_cran("qs2", type = "source", configure.args = "--with-TBB")
+# remotes::install_cran("qs2", type = "source", configure.args = "--with-simd=AVX2")
 
 ## ----eval=FALSE---------------------------------------------------------------
 # file_qs2 <- tempfile(fileext = ".qs2")
@@ -44,6 +41,31 @@ IS_GITHUB <- Sys.getenv("IS_GITHUB") != ""
 ## ----eval=FALSE---------------------------------------------------------------
 # qs_save(data, "myfile.qs2")
 # data <- qs_read("myfile.qs2", validate_checksum = TRUE)
+
+## ----eval=FALSE---------------------------------------------------------------
+# x <- serialize(mtcars, connection = NULL)
+# xz <- zstd_compress_raw(x, compress_level = 3)
+# x2 <- zstd_decompress_raw(xz)
+# stopifnot(identical(x, x2))
+
+## ----eval=FALSE---------------------------------------------------------------
+# infile <- tempfile()
+# writeBin(as.raw(1:5), infile)
+# zfile <- tempfile(fileext = ".zst")
+# zstd_compress_file(infile, zfile, compress_level = 1)
+# outfile <- tempfile()
+# zstd_decompress_file(zfile, outfile)
+# stopifnot(identical(readBin(infile, "raw", 5), readBin(outfile, "raw", 5)))
+
+## ----eval=FALSE---------------------------------------------------------------
+# # library(data.table)
+# save_file <- tempfile(fileext = ".csv.zst")
+# 
+# # write out zstd compressed table
+# zstd_out(data.table::fwrite, mtcars, file = save_file)
+# 
+# # read in zstd compressed table
+# dt <- zstd_in(data.table::fread, file = save_file)
 
 ## ----eval=FALSE---------------------------------------------------------------
 # qd_save(data, "myfile.qs2")
